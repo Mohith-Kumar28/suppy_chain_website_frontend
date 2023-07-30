aebbbcbef7f9a8dc5d709e092351882270c3efe5
Mohith kumar <murugan28aug@gmail.com>
2023-07-30 10:24:44 +0530

    footer

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
   <div className="bg-gray-800 bg-opacity-75">
     <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
       <p className="text-gray-400 text-sm text-center sm:text-left">© 2023 Boujee —
-        <a href="https://twitter.com/knyttneve" className="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer">@mail</a>
+        <a href="https://twitter.com/knyttneve" className="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer">Sokandar@boujee.ltd</a>
       </p>
-      <span className="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-400 text-sm">Enamel pin tousled raclette tacos irony</span>
+      {/* <span className="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-400 text-sm">Enamel pin tousled raclette tacos irony</span> */}
     </div>
   </div>
 </footer>
